2f4b61c38a2908d8e39f942fb8fb7ccf89f35ac3
nurgi17 <usen.nurgisa@gmail.com>
2020-06-23 13:48:31 +0600

    #2 Search the photos with Flickr API

---
+++ vue-flickr/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+
+Vue.config.productionTip = false
+Vue.use(axios)
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
