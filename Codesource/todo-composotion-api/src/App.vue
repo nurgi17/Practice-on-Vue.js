2d4c00a821c24250574f5b57c5db3cf8351bb828
nurgi17 <usen.nurgisa@gmail.com>
2020-08-19 12:59:19 +0600

    Todo app with vue3 composition api

---
+++ Codesource/todo-composotion-api/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="app">
+    <section class="container">
+      <div class="card">
+        <div class="card-header">
+          <h3>Add New Todo</h3>
+        </div>
+        <div class="card-body">
+          <input type="text" class="form-control" v-model="state.todo" required/>
+          <button class="btn btn-primary my-2" @click="addNewTodo">Add Todo</button>
+        </div>
+      </div>
+
+      <div class="container my-5">
+        <div class="card">
+          <div class="card-header">
+            <h3>MtTodos</h3>
+          </div>
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item" v-for="(todo, i) in state.todos" :key="i">
+                {{ todo }} <i class="fa fa-trash" aria-hidden="true" @click="removeTodo(i)"></i>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { reactive } from '@vue/composition-api'
+
+export default {
+  setup () {
+    const { state, addNewTodo, removeTodo } = useTodo()
+    return {
+      state,
+      addNewTodo,
+      removeTodo
+    }
+  }
+}
+function useTodo () {
+  const state = reactive({
+    todo: '',
+    todos: []
+  })
+  function addNewTodo () {
+    if (state.todo !== '') {
+      state.todos.push(state.todo)
+      state.todo = ''
+    } else {
+      alert('You should enter something')
+    }
+  }
+  function removeTodo (i) {
+    state.todos.splice(i, 1)
+  }
+  return {
+    state,
+    addNewTodo,
+    removeTodo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.fa {
+  color:red;
+  margin-left: 20px;
+}
+.fa:hover{
+  color: black;
+}
+</style>
