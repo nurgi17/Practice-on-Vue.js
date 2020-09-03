57bf962c6de9a082ffb2be57aeec8e66fc6e68cf
nurgi17 <usen.nurgisa@gmail.com>
2020-09-03 12:24:29 +0600

    Simple todo app with quasar framework

---
+++ Quasar/quasar-todo/src/pages/Help.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid eius atque quas inventore officia. Reiciendis quaerat quasi autem tempora, accusamus excepturi temporibus accusantium facilis fugiat ut nulla eveniet libero consequatur!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid eius atque quas inventore officia. Reiciendis quaerat quasi autem tempora, accusamus excepturi temporibus accusantium facilis fugiat ut nulla eveniet libero consequatur!</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
